65c9cbb1da63253ec53f903ff2bb7cb20a8c25fe
Julien PAUSE <julien.pause@epitech.eu>
2021-03-30 15:16:41 +0400

    refactor(Core main): catch exceptions thrown

--- lib/Core/src/main.cpp
+++ lib/Core/src/main.cpp
@@ -6,16 +6,24 @@
 */
 
 #include <iostream>
+#include <memory>
 #include "Core.hpp"
+#include "Error.hpp"
 
 int main(int ac, char **av) {
-    arc::Core core;
+    std::unique_ptr<arc::Core> core = nullptr;
 
     if (ac != 2) {
         std::cerr << USAGE << std::endl;
         return MY_EXIT_FAILURE;
     }
     (void)av;
-    core.run();
+    try {
+        core = std::unique_ptr<arc::Core>(new arc::Core);
+        core->run();
+    } catch (const arc::Error &err) {
+        std::cerr << err.what() << std::endl;
+        return MY_EXIT_FAILURE;
+    }
     return 0;
 }
